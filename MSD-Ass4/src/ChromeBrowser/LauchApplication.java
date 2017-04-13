ccf07b1aa217902bd0a5a7f7a1ff5b72038c8e9c
kishore <mgorti@unomha.edu>
2017-04-13 17:28:21 -0500

    first commit

---
+++ MSD-Ass4/src/ChromeBrowser/LauchApplication.java
@@ -0,0 +1,16 @@
+package ChromeBrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LauchApplication {
+
+	public static void main(String[] args)
+	{
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Selenium Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://omk.herokuapp.com");
+	}
+
+}
